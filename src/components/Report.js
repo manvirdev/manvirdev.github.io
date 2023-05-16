a9de3da6b414f16fb516833fcb566e49c7304ab8
Manvir Singh Heer <manvirheer007@gmail.com>
2023-05-15 19:48:14 -0700

    refactor: fix the starting notes

--- src/components/Report.js
+++ src/components/Report.js
@@ -22,56 +22,70 @@ const MyComponent = () => {
   }
 
   const strProcessor = (str) => {
-    let officers = [];
-    let patrols = [];
-
-    str = String(str);
-    
-    str = str.replace(/(?:\r\n|\r|\n)/g, ' ');
-    str = str.replace(/\t+/gm, ' ')
-
-    str = str.trim();
-
-    const arr = str.split(' View Tour Session + ').reverse();
-
-    arr.forEach((ele) => {
-      ele = ele.replace('+ S - M232 - Colliers - Exchange Building  + ', '');
-
-      const patrolType = ele.match(/^([\w\/\-]+)/)[0];
-
-      ele = ele.replace(/^([\w\/\-]+)/, '');
-      ele = ele.replace(/\sPatrol\s/, '');
-
-      const officerFirstName = ele.match(/\w+\s+/)[0].trim();
-      ele = ele.replace(/\w+\s+/, '');
-
-      // Last name in all caps
-      const officerLastName = ele.match(/\w+/)[0].toUpperCase();
-      ele = ele.replace(/\w+/, '');
-
-      const officer = new Person(officerFirstName, officerLastName);
-
-      const exists = officers.some(ele => ele.isSamePerson(officer));
-
-      if (!exists) {
-        officers.push(officer);
+    try {
+      let officers = [];
+      let patrols = [];
+  
+      str = String(str);
+      
+      str = str.replace(/(?:\r\n|\r|\n)/g, ' ');
+      str = str.replace(/\t+/gm, ' ');
+  
+      str = str.trim();
+  
+      const arr = str.split(' View Tour Session + ').reverse();
+  
+      arr.forEach((ele) => {
+        ele = ele.replace('+ S - M232 - Colliers - Exchange Building  + ', '');
+  
+        const patrolType = ele.match(/^([\w\/\-]+)/)[0];
+  
+        ele = ele.replace(/^([\w\/\-]+)/, '');
+        ele = ele.replace(/\sPatrol\s/, '');
+  
+        const officerFirstName = ele.match(/\w+\s+/)[0].trim();
+        ele = ele.replace(/\w+\s+/, '');
+  
+        // Last name in all caps
+        const officerLastName = ele.match(/\w+/)[0].toUpperCase();
+        ele = ele.replace(/\w+/, '');
+  
+        const officer = new Person(officerFirstName, officerLastName);
+  
+        const exists = officers.some(ele => ele.isSamePerson(officer));
+  
+        if (!exists) {
+          officers.push(officer);
+        }
+  
+        let backupOfficer = officers.find((o) => !o.isSamePerson(officer));
+  
+        const startTime = moment(ele.match(/\d+:\d+/)[0], 'HH:mm');
+        const endTime = moment(ele.match(/\d+:\d+/g)[1], 'HH:mm');
+  
+        let patrol = new Patrol(patrolType, officer, backupOfficer, startTime, endTime);
+        
+        patrols.push(patrol);
+      });
+  
+      if (patrols.length === 0) {
+        throw new Error('No patrols found.');
       }
-
-      let backupOfficer = officers.find((o) => !o.isSamePerson(officer));
-
-      const startTime = moment(ele.match(/\d+:\d+/)[0], 'HH:mm');
-      const endTime = moment(ele.match(/\d+:\d+/g)[1], 'HH:mm');
-
-      let patrol = new Patrol(patrolType, officer, backupOfficer, startTime, endTime);
-      
-      patrols.push(patrol);
-
-      let shift = new Shift(new Person("test", "test"), patrols[0].startTime, patrols[patrols.length - 1].endTime, officers, patrols);
-      
-      setResultValue(shift.generateReport());
-    })
+  
+      let shift = new Shift(new Person("Navdeep", "Kaur"), patrols[0].startTime, patrols[patrols.length - 1].endTime, officers, patrols);
+      shift.generateReport()
+      .then((report) => {
+       setResultValue(report);
+        // Process the generated report here
+      })
+      .catch((error) => {
+        console.error("Error generating report:", error);
+      });
+    } catch (error) {
+      setResultValue('Unable to generate report. Got this error: ' + error.message);
+    }
   };
-
+  
   const containerStyles = {
     minHeight: '100vh',
     display: 'flex',
--- src/components/reports/Shift.js
+++ src/components/reports/Shift.js
@@ -1,5 +1,6 @@
 // Shift.js
 import moment from "moment";
+import axios from "axios";
 
 class Shift {
     constructor(briefingOfficer, startTime, endTime, officers, patrols) {
@@ -9,57 +10,96 @@ class Shift {
         this.officers = officers;
         this.patrols = patrols;
     }
+    
+    async getWeatherData(city) {
+        const apiKey = "0b5dec097d48d73f2e8e06cde9bab8fd";
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=49.28&lon=123.12&appid=${apiKey}`;
+    
+        try {
+          const response = await axios.get(apiUrl);
+          const weatherData = response.data;
+          return weatherData;
+        } catch (error) {
+          console.error("Error fetching weather data:", error);
+          return null;
+        }
+      }
+    async generateReport() {
+        const report = [];
+        const weatherData = await this.getWeatherData("New York");
+        report.push(weatherData);
 
- generateReport() {
-  const report = [];
+        report.push(...this.getStartingNotes())
+        this.patrols.forEach((patrol) => {
+          const timeDiff = this.calculateTimeDifference(patrol);
+          const { status, additionalLine } = this.getStatusAndAdditionalLine(patrol, timeDiff);
+          const patrolString = this.getPatrolString(patrol, status, additionalLine);
+      
+          report.push(patrolString);
+        });
+      
+        return report.join('');
+      }
+      
+      calculateTimeDifference(patrol) {
+        const duration = moment.duration(patrol.endTime.diff(patrol.startTime));
+        const hours = duration.hours();
+        const minutes = duration.minutes();
+        return hours * 60 + minutes;
+      }
+      
+      getStatusAndAdditionalLine(patrol, timeDiff) {
+        let status;
+        let additionalLine = '';
+        const cctvStartTime = patrol.startTime.clone().add(5, 'minutes');
+        const backupOfficer = this.officers.find((officer) => !officer.isSamePerson(patrol.officer));
+      
+        if (timeDiff < 30) {
+          status = 'completed quickly';
+        } else if (timeDiff < 60) {
+          status = 'completed within an hour';
+          if (backupOfficer != null) {
+            additionalLine = `${cctvStartTime.format('HH:mm')}: S/O ${backupOfficer.firstName} ${backupOfficer.lastName.toUpperCase()} watching CCTV cameras.\n`;
+          }
+        } else {
+          status = 'completed in a longer duration';
+          if (backupOfficer != null) {
+            additionalLine = `${cctvStartTime.format('HH:mm')}: S/O ${backupOfficer.firstName} ${backupOfficer.lastName.toUpperCase()} watching CCTV cameras.\n`;
+          }
+        }
+      
+        return { status, additionalLine };
+      }
+      
+      getPatrolString(patrol, status, additionalLine) {
+        const startString = `${patrol.startTime.format('HH:mm')}: S/O ${patrol.officer.firstName} ${patrol.officer.lastName.toUpperCase()} started the ${patrol.type} patrol.\n`;
+        const endString = `${patrol.endTime.format('HH:mm')}: S/O ${patrol.officer.firstName} ${patrol.officer.lastName.toUpperCase()} completed the ${patrol.type}, all clear.\n`;
+        return startString + additionalLine + endString;
+      } 
 
-  this.patrols.forEach((patrol) => {
-    const timeDiff = this.calculateTimeDifference(patrol);
-    const { status, additionalLine } = this.getStatusAndAdditionalLine(patrol, timeDiff);
-    const patrolString = this.getPatrolString(patrol, status, additionalLine);
-
-    report.push(patrolString);
-  });
-
-  return report.join('');
-}
-
-calculateTimeDifference(patrol) {
-  const duration = moment.duration(patrol.endTime.diff(patrol.startTime));
-  const hours = duration.hours();
-  const minutes = duration.minutes();
-  return hours * 60 + minutes;
-}
-
-getStatusAndAdditionalLine(patrol, timeDiff) {
-  let status;
-  let additionalLine = '';
-  const cctvStartTime = patrol.startTime.clone().add(5, 'minutes');
-  const backupOfficer = this.officers.find((officer) => !officer.isSamePerson(patrol.officer));
-
-  if (timeDiff < 30) {
-    status = 'completed quickly';
-  } else if (timeDiff < 60) {
-    status = 'completed within an hour';
-    if (backupOfficer != null) {
-      additionalLine = `${cctvStartTime.format('HH:mm')}: S/O ${backupOfficer.firstName} ${backupOfficer.lastName.toUpperCase()} watching CCTV cameras.\n`;
-    }
-  } else {
-    status = 'completed in a longer duration';
-    if (backupOfficer != null) {
-      additionalLine = `${cctvStartTime.format('HH:mm')}: S/O ${backupOfficer.firstName} ${backupOfficer.lastName.toUpperCase()} watching CCTV cameras.\n`;
-    }
-  }
-
-  return { status, additionalLine };
-}
-
-getPatrolString(patrol, status, additionalLine) {
-  const startString = `${patrol.startTime.format('HH:mm')}: S/O ${patrol.officer.firstName} ${patrol.officer.lastName.toUpperCase()} started the ${patrol.type} patrol.\n`;
-  const endString = `${patrol.endTime.format('HH:mm')}: S/O ${patrol.officer.firstName} ${patrol.officer.lastName.toUpperCase()} completed the ${patrol.type}, all clear.\n`;
-  return startString + additionalLine + endString;
-}
-
+      getStartingNotes() {
+        const startingNotes = [];
+        const briefingTime = this.startTime.format("HH:mm");
+      
+        // Start time: S/O briefing officer briefed all the officers' names
+        const briefingNote = `${briefingTime}: S/O ${this.briefingOfficer.firstName} ${this.briefingOfficer.lastName.toUpperCase()} briefed ${this.getOfficersList()} and handed over the keys.\n`;
+      
+        startingNotes.push(briefingNote);
+      
+        // Start time + 10 mins: All the officers read the passon
+        const readPassonTime = this.startTime.clone().add(10, "minutes").format("HH:mm");
+        const officersList = this.getOfficersList();
+        const readPassonNote = `${readPassonTime}: ${officersList} read the pass-on and emails.\n`;
+      
+        startingNotes.push(readPassonNote);
+      
+        return startingNotes;
+      }
+      
+      getOfficersList() {
+        return this.officers.map((officer) => `S/O ${officer.firstName} ${officer.lastName.toUpperCase()}`).join(", ");
+      }
+      
       
 }
 
